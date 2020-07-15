9fe3066dc6e95145302fa82f1dfbd33696797384
Chih-Ming Louis Lee <louis@chihminglee>
2020-07-15 00:34:21 -0500

    wrap parse functions in class

---
+++ example/test.md
@@ -0,0 +1,11 @@
+# Level 1
+- 2: value
+- list:
+  * item 1
+  * item 2
+- test: a
+- 3: value
+- 4: value
+  > commetn!
+- 5: value
+- 6: value
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
+const { MdFile, parseMarkdata } = require('./parser.js');
 const util = require('util');
-
-const { parseMarkdata } = require('./parser.js');
 
 const filePath = './example/test.md';
 const fileText = fs.readFileSync(filePath, 'utf8');
 
-const result = parseMarkdata(fileText);
+const mdFile = new MdFile(fileText);
+
+console.log(mdFile.fileArray)
+result = parseMarkdata(fileText);
 console.log(util.inspect(result, false, null, true));
-console.log(result);
