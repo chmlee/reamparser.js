70796a751e4e4c3939096ce1bf0c19e6c99d24bd
Chih-Ming Louis Lee <louis@chihminglee.com>
2020-07-17 16:11:51 -0500

    clear

--- playground_test.js
+++ playground_test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const util = require('util');
 
-const { MdFile } = require('./src/markdata.js');
+const { MdFile } = require('./dist/all.js');
 
 const filePath = './example/eu.md';
 const fileText = fs.readFileSync(filePath, 'utf8');
---
+++ src/app.js
@@ -0,0 +1,1 @@
+const test = () => console.log(123);
