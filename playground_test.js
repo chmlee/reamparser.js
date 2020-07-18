1e12a93d6032af579c6b3eb025634c4522747035
Chih-Ming Louis Lee <louis@chihminglee.com>
2020-07-17 19:59:57 -0500

    webpack + babel

--- playground_test.js
+++ playground_test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const util = require('util');
 
-const { MdFile } = require('./dist/all.js');
+const { MdFile } = require('./dist/markdata.min.js');
 
 const filePath = './example/eu.md';
 const fileText = fs.readFileSync(filePath, 'utf8');
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/markdata.js',
+  output: {
+    filename: 'markdata.min.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'markdata',
+    libraryTarget: 'umd',
+    globalObject: 'this', // https://medium.com/@JakeXiao/window-is-undefined-in-umd-library-output-for-webpack4-858af1b881df
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
